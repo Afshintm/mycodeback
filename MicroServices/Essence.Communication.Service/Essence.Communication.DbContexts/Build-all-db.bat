957a0ef7bcbf3234708a064109ff58409ebb0d11
Afshin Teymoori <afshin.teymoori@homestay.care>
2019-02-05 15:39:19 +1100

    Unified build database in one batch file on Microservices folder

--- MicroServices/Essence.Communication.Service/Essence.Communication.DbContexts/Build-all-db.bat
+++ MicroServices/Essence.Communication.Service/Essence.Communication.DbContexts/Build-all-db.bat
@@ -1,7 +1,8 @@
+Echo Better to use MicroServices/build-all-db
 
 REM dotnet ef migrations add ApplicationDatabase -c ApplicationDbContext -o Data/Migrations/ApplicationDatabase --startup-project ../Essence.Communication.Api
 
 REM dotnet ef migrations script -c ApplicationDbContext -o Data/Migrations/ApplicationDatabase.sql --startup-project ../Essence.Communication.Api
 
-dotnet ef database  update --context ApplicationDbContext --startup-project ../Essence.communication.api
+REM dotnet ef database  update --context ApplicationDbContext --startup-project ../Essence.communication.api
 
--- MicroServices/Identity.Management.Service/Identity.Management.Api/Build-all-db.bat
+++ MicroServices/Identity.Management.Service/Identity.Management.Api/Build-all-db.bat
@@ -1,21 +1,22 @@
+Echo Better to use MicroServices/build-all-db
 REM dotnet ef migrations add ApplicationIdentity  -c ApplicationIdentityDbContext  -o Data/Migrations/ApplicationIdentityDb
 
 REM dotnet ef migrations script -c ApplicationIdentityDbContext -o Data/Migrations/ApplicationIdentityDb.sql
 
-dotnet ef database  update -c ApplicationIdentityDbContext 
+REM dotnet ef database  update -c ApplicationIdentityDbContext 
 
 
 REM dotnet ef migrations add Grants -c PersistedGrantDbContext -o Data/Migrations/PersistedGrantDb
 
 REM dotnet ef migrations script -c PersistedGrantDbContext -o Data/Migrations/PersistedGrantDb.sql
 
-dotnet ef database update -c PersistedGrantDbContext
+REM dotnet ef database update -c PersistedGrantDbContext
 
 
 REM dotnet ef migrations add Config -c ConfigurationDbContext -o Data/Migrations/ConfigurationDb
 
 REM dotnet ef migrations script -c ConfigurationDbContext -o Data/Migrations/ConfigurationDb.sql
 
-dotnet ef database update -c ConfigurationDbContext
+REM dotnet ef database update -c ConfigurationDbContext
 
-dotnet run /seed
+REM dotnet run /seed
