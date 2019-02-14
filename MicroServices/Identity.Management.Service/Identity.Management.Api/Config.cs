db268dc8f1d9507d6926fa521006029f7a66705a
Afshin Teymoori <afshin.teymoori@homestay.care>
2019-02-14 14:00:31 +1100

    Add angular client to the identity server application

--- MicroServices/Identity.Management.Service/Identity.Management.Api/Config.cs
+++ MicroServices/Identity.Management.Service/Identity.Management.Api/Config.cs
@@ -102,7 +102,40 @@ namespace Identity.Management.Api
                     },
                     AllowOfflineAccess = true,
                     AlwaysSendClientClaims = true,
-                }
+                },
+                new Client
+                    {
+                        ClientName = "angularclient",
+                        ClientId = "angularclient",
+                        AccessTokenType = AccessTokenType.Reference,
+                        //AccessTokenLifetime = 600, // 10 minutes, default 60 minutes
+                        AllowedGrantTypes = GrantTypes.Implicit,
+                        AllowAccessTokensViaBrowser = true,
+                        RedirectUris = new List<string>
+                        {
+                            "http://localhost:4200/auth-callback"
+
+                        },
+                        PostLogoutRedirectUris = new List<string>
+                        {
+                            "http://localhost:4200/Unauthorized"
+                        },
+                        AllowedCorsOrigins = new List<string>
+                        {
+                            "http://localhost:4200"
+                        },
+                        AllowedScopes = new List<string>
+                        {
+                            "openid",
+                            "profile",
+                            "email",
+                            "roles"
+                            //"dataEventRecords",
+                            //"dataeventrecordsscope",
+                            //"securedFiles",
+                            //"securedfilesscope",
+                        }
+                    }
             };
         }
 
